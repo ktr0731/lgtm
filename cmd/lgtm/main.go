59d8eb5a3e3cfb14491fe396efe71091f8e67cfb
ktr0731 <aizu.s1230022@gmail.com>
2018-05-04 21:01:59 +0900

    add command-line options

--- cmd/lgtm/main.go
+++ cmd/lgtm/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/ktr0731/lgtm"
 )
 
+var (
+	w = flag.Bool("w", false, "write to input file")
+)
+
 func main() {
-	r, err := os.Open("example.gif")
+	flag.Parse()
+
+	switch {
+	case *w && len(flag.Args()) != 1:
+		fmt.Println("usage: lgtm -w <input.gif>")
+		os.Exit(1)
+	case !*w && len(flag.Args()) != 2:
+		fmt.Println("usage: lgtm <input.gif> <output.gif>")
+		os.Exit(1)
+	}
+
+	var r io.Reader
+	var err error
+	f, err := os.Open(flag.Arg(0))
 	if err != nil {
 		panic(err)
 	}
-	defer r.Close()
+	defer f.Close()
+	r = f
 
-	w, err := os.Create("out.gif")
+	var outFileName string
+	if *w {
+		buf := new(bytes.Buffer)
+		io.Copy(buf, f)
+		r = buf
+		f.Close()
+
+		outFileName = flag.Arg(0)
+	} else {
+		outFileName = flag.Arg(1)
+	}
+
+	w, err := os.Create(outFileName)
 	if err != nil {
 		panic(err)
 	}
